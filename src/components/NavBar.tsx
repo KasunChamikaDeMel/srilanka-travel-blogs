981c5b8fea640466f35b4670dd98189240e7366d
Kasun Chamika De Mel <chamikakasun33635@gmail.com>
2025-12-10 13:34:08 +0530

    minor changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
 import Link from "next/link";
-import Image from "next/image";
 import MaxWidthWrapper from "./MaxWidthWrapper";
 import { NavigationMenuBace } from "./NavMenu";
 import {
